2c630b303df2f92c8c558c3a52fa23399ec0029c
nholvoet <niels.holvoet@gmail.com>
2016-12-07 20:42:07 +0100

    Looping files with users

--- src/main/scala/be/nille/App.scala
+++ src/main/scala/be/nille/App.scala
@@ -5,6 +5,8 @@
  */
 
 package be.nille
+import java.io.File
+import scala.io.Source
 
 object App {
 
@@ -12,7 +14,27 @@ object App {
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println("Starting program")
+   
+    getListOfFiles(getClass.getClassLoader.getResource("files").getPath)
+    .map(file => {
+        println(file.getName + "\n");
+        Source.fromFile(file).getLines.map(User(_)).toList
+      })
+    .reduceLeft(_ ++ _)// merge the 2 lists
+    .foreach(println)
+    
+    println("Ending program")
+    
+  }
+
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
   }
 
 }
